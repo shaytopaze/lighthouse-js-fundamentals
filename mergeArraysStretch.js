0abceb2fa3063b56974915a54eeed4a668dd830b
Shay Alexander <shaytopazehair@gmail.com>
2017-11-09 23:31:34 +0000

    Merge sorted arrays

---
+++ mergeArraysStretch.js
@@ -0,0 +1,13 @@
+function merge(array1, array2) {
+  var sorted = array1.concat(array2).sort(function(array1, array2) {
+    return array1 - array2;
+  });
+
+  return sorted;
+
+
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
